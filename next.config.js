6cab10a31313a7fcb2b6174e757cde9efdbc490a
Hari CM <chemmangathari@gmail.com>
2026-02-18 08:57:11 +0530

    chore: build fix

--- next.config.js
+++ next.config.js
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  experimental: {
+    optimizePackageImports: ['@chemmangat/optimistic-update'],
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+      }
+    }
+    return config
+  },
+}
 
 module.exports = nextConfig
